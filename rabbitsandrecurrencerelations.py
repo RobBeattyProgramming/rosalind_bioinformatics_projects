a2475639f594eca4a9e597918a7d611269d431ff
robbeattyprogramming <robbeattyprogramming@gmail.com>
2026-03-30 00:34:23 -0400

    created loop to track pattern

--- rabbitsandrecurrencerelations.py
+++ rabbitsandrecurrencerelations.py
@@ -1,28 +1,16 @@
 nMonths = int(input())
 kRabbitPairs = int(input())
 
-"""The population begins in the first month with a pair of newborn rabbits.
 
-Rabbits reach reproductive age after one month.
 
-In any given month, every rabbit of reproductive age mates with another rabbit of reproductive age.
-
-Exactly one month after two rabbits mate, they produce one male and one female rabbit.
-
-Rabbits never die or stop reproducing."""
-
-"""
-1. collect number of months (n) and number of rabbit pairs
-2. iterate over length of months
-3. account for amount of pairs able to reproduce"""
 
 """
 
 
-1 2 3 4 5
+            1 2 3 4 5
 
-1 4 8 19
-
+total:      1 1 4 7 19
+breedable:  0 1 1 4
 
 
 **** LOOP AND HOLD VALUE OF PREVIOUS MONTH to multiply by
@@ -30,14 +18,28 @@ Rabbits never die or stop reproducing."""
 
 
 """
+
 monthlyPattern = []
 nowAgedRabbits = 0
+totalRabbits = 1
 
 
-for i in range (0, nMonths):
+for i in range (0, nMonths - 1):
     if i == 0:
         monthlyPattern.append(1)
         nowAgedRabbits = 1
+    if i == 1:
+        monthlyPattern.append(1)
+        pass
+    else:
+        totalRabbits = (nowAgedRabbits * kRabbitPairs) + totalRabbits
+        monthlyPattern.append(totalRabbits)
+        nowAgedRabbits = totalRabbits - (nowAgedRabbits * kRabbitPairs)
+
+
+
+
+print(monthlyPattern)
 
     
     
